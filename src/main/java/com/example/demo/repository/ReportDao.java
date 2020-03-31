842d7f32513824536dabea7bc40d19045b00f2cc
stakagi <Shinichi.Takagi@ha.ax>
2020-03-31 09:12:41 +0300

    Getting ready to eliminate those pesky anomalies

--- src/main/java/com/example/demo/repository/ReportDao.java
+++ src/main/java/com/example/demo/repository/ReportDao.java
@@ -6,9 +6,11 @@ import java.util.Optional;
 import com.example.demo.entity.Report;
 
 public interface ReportDao {
+	
 	List<Report> findAll();
 	Optional<Report> findById(int id);
 	void insert(Report report);
 	int update(Report report);
 	int deleteById(int id);
+	
 }
