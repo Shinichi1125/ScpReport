2f84ce4dcedd38ea332849c17e17f9ce5a8244a9
stakagi <Shinichi.Takagi@ha.ax>
2020-04-30 15:35:36 +0300

    adding DBFileRepositoryImpl class

---
+++ src/main/java/com/example/demo/repository/DBFileRepositoryImpl.java
@@ -0,0 +1,21 @@
+package com.example.demo.repository;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.demo.model.DBFile;
+
+public class DBFileRepositoryImpl implements DBFileRepository {
+
+	@Override
+	public DBFile storeFile(MultipartFile file) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DBFile getFile(String fileId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
