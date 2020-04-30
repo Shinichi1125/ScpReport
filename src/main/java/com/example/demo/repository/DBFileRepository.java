eba6a7b52cd47b4144449476ff5e98dbc2212fa4
stakagi <Shinichi.Takagi@ha.ax>
2020-04-30 15:31:43 +0300

    resutructure DBFileRepository interface so it works with JDBC Template

--- src/main/java/com/example/demo/repository/DBFileRepository.java
+++ src/main/java/com/example/demo/repository/DBFileRepository.java
@@ -1,10 +1,9 @@
 package com.example.demo.repository;
 
+import org.springframework.web.multipart.MultipartFile;
 import com.example.demo.model.DBFile;
-import org.springframework.data.jdbc.repository.JdbcRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface DBFileRepository extends JdbcRepository<DBFile, String> {
-
+public interface DBFileRepository {
+	DBFile storeFile(MultipartFile file);
+	DBFile getFile(String fileId);
 }
